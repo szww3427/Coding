f8ab6e1b1c4fd5f6a42d9678a6dd335826e27e76
wangweiw <wangweiw@cn.ibm.com>
2018-06-08 21:58:31 +0800

    java swing

---
+++ src/main/java/com/bupt/ww/HelloWorldSwing.java
@@ -0,0 +1,66 @@
+package com.bupt.ww;
+
+import javax.swing.*;
+
+public class HelloWorldSwing {
+
+    public static void main(String[] args){
+        initWindow();
+    }
+
+    public static void initWindow(){
+        JFrame jFrame = new JFrame("Hello Login");
+
+        jFrame.setSize(350,200);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel jPanel = new JPanel();
+
+        jFrame.add(jPanel);
+
+        placeComponent(jPanel);
+
+        jFrame.setVisible(true);
+    }
+
+    private static void placeComponent(JPanel panel) {
+        /* 布局部分我们这边不多做介绍
+         * 这边设置布局为 null
+         */
+        panel.setLayout(null);
+
+        // 创建 JLabel
+        JLabel userLabel = new JLabel("User:");
+        /* 这个方法定义了组件的位置。
+         * setBounds(x, y, width, height)
+         * x 和 y 指定左上角的新位置，由 width 和 height 指定新的大小。
+         */
+        userLabel.setBounds(10,20,80,25);
+        panel.add(userLabel);
+
+        /*
+         * 创建文本域用于用户输入
+         */
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100,20,165,25);
+        panel.add(userText);
+
+        // 输入密码的文本域
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(10,50,80,25);
+        panel.add(passwordLabel);
+
+        /*
+         *这个类似用于输入的文本域
+         * 但是输入的信息会以点号代替，用于包含密码的安全性
+         */
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(100,50,165,25);
+        panel.add(passwordText);
+
+        // 创建登录按钮
+        JButton loginButton = new JButton("login");
+        loginButton.setBounds(10, 80, 80, 25);
+        panel.add(loginButton);
+    }
+}
---
+++ src/test/java/com/bupt/ww/HelloWorldSwingTest.java
@@ -0,0 +1,12 @@
+package com.bupt.ww;
+
+import org.junit.Test;
+
+public class HelloWorldSwingTest {
+
+    @Test
+    public void initWondow() {
+        HelloWorldSwing helloWorldSwing = new HelloWorldSwing();
+        helloWorldSwing.initWindow();
+    }
+}
